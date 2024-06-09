e020e3755cb9d070a321fead05c20f12454dd1dc
Ashura2003 <shreejanm21@gmail.com>
2024-06-09 11:42:26 +0545

    up to date

--- src/apis/Api.js
+++ src/apis/Api.js
@@ -9,6 +9,13 @@ const Api = axios.create({
   },
 });
 
+// Make a config for token
+const config = {
+  headers: {
+    authorization: `Bearer ${localStorage.getItem("token")}`,
+  },
+};
+
 // Test API
 export const testAPI = () => Api.get("/test");
 
@@ -21,15 +28,16 @@ export const loginUserApi = (data) => Api.post("/api/user/login", data);
 // create product Api
 export const createProductApi = (data) => Api.post("/api/product/create", data);
 
-export const getProductsApi = () => Api.get("/api/product/get_all_products");
+export const getProductsApi = () =>
+  Api.get("/api/product/get_all_products", config);
 
 export const getSingleProductApi = (id) =>
-  Api.get(`/api/product/get_single_product/${id}`);
+  Api.get(`/api/product/get_single_product/${id}`, config);
 
 export const deleteProduct = (id) =>
-  Api.delete(`/api/product/delete_product/${id}`);
+  Api.delete(`/api/product/delete_product/${id}`, config);
 
 export const updateProductApi = (id, data) =>
-  Api.put(`/api/product/update_product/${id}`, data);
+  Api.put(`/api/product/update_product/${id}`, data, config);
 
 //http://localhost:5000/test
